1b06333b0c28f522efdb8373d2ddba6bdb6fd0be
mdibrahim098 <67840334+mdibrahim098@users.noreply.github.com>
2025-10-23 20:27:02 +0600

    Change 9 , create application db context

---
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CodingWiki_Model.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodingWiki_DataAccess.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
---
+++ CodingWiki_Model/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace CodingWiki_Model.Models
+{
+    public class Book
+    {
+        public int IDBook { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public double Price { get; set; }
+
+    }
+}
