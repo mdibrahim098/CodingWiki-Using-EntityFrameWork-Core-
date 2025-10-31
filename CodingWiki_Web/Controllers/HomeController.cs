7976b58b39834cea33f7cd683f8a55b6e16bd0ea
mdibrahim098 <67840334+mdibrahim098@users.noreply.github.com>
2025-10-31 18:53:06 +0600

    Change 29 ---

--- CodingWiki_Web/Controllers/HomeController.cs
+++ CodingWiki_Web/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CodingWiki_Web.Controllers
 {
-    public class HomeController : Controller
+     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
